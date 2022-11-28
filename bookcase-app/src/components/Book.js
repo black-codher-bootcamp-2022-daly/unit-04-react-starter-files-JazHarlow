43fea9f5ad45a0861f099dbdf23169a3a96bf1bd
JazHarlow <Jazreenaharlow@gmail.com>
2022-11-28 21:07:11 +0000

    added class task

--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-function Book() {
-  return "This is the output of a book component. Replace this string with code to display an individual book's attributes.";
+// function Book(props) {
+
+// //   // console.log(props);
+// //   return <div> 
+// //     <h2>{props.book.volumeInfo.title}</h2>
+// //     <p>Author: {props.book.volumeInfo.authors}</p>
+// //     </div>
+// // }
+
+function Book({book}) {
+  const { title, authors } = book.volumeInfo;
+  const { retailPrice } = book.saleInfo;
+  console.log(book)
+  return (
+    <div style={{
+      backgroundColor: 'pink'
+    }}>
+      <h3>Title: {title}</h3>
+      <p>Author: {authors}</p>
+      <p>{retailPrice && retailPrice.amount}</p>
+    </div>
+  );
 }
 
 export default Book;
